cbc8b7bce740a3d556129c8e1a853c6a73bfef95
heeseon99 <gmltjs418@gmail.com>
2024-01-17 14:51:03 +0900

    11382번. 꼬마 정민
    A+B+C 계산

---
+++ src/beakjoon_problem/beakjoon_11382.java
@@ -0,0 +1,17 @@
+package beakjoon_problem;
+
+import java.util.Scanner;
+
+public class beakjoon_11382 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+        long num1 = scanner.nextLong();
+		long num2 = scanner.nextLong();
+		long num3 = scanner.nextLong();
+
+        System.out.println(num1 + num2 + num3);
+
+		scanner.close();
+	}
+}
