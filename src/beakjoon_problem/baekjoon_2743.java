de4aebd8c9d54ded76c9c20648d191ccc1e496d4
heeseon99 <gmltjs418@gmail.com>
2024-01-30 00:37:54 +0900

    2743번. 단어 길이 재기
    알파벳으로만 이루어진 단어를 입력 받아 그 길이를 출력하는 프로그램

---
+++ src/beakjoon_problem/baekjoon_2743.java
@@ -0,0 +1,14 @@
+package beakjoon_problem;
+
+import java.util.Scanner;
+
+public class baekjoon_2743 {
+    public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+        String word = scanner.nextLine();
+        System.out.println(word.length());
+
+		scanner.close();
+	}    
+}
